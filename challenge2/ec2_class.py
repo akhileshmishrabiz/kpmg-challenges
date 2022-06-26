480e208692c429f89cdab82460ae9c985c460be1
Akhilesh mishra <akhileshmishrabiz@gmail.com>
2022-06-26 16:10:32 +0530

    added object oriented programming in challenge2

---
+++ challenge2/ec2_class.py
@@ -0,0 +1,57 @@
+class ec2:
+    import json
+    import requests
+
+
+    meta_data_url = 'http://169.254.169.254/latest/'
+
+    ## below function will validate if input is a valid json or not
+    def json_validator(json_input):
+        try:
+            ec2.json.loads(json_input)
+        except ValueError:
+            return False
+        return True
+
+
+    def walk_the_tree(url,arr):
+        output={}
+        
+        for item in arr:
+            updated_url= url+item
+            response = ec2.requests.get(updated_url)
+            text=response.text 
+            if item[-1] == '/':
+                next_level_items = text.splitlines()
+                output[item[:-1]]= ec2.walk_the_tree(updated_url,next_level_items)
+            elif ec2.json_validator(text):
+                output[item] = ec2.json.loads(text)
+            else:
+                output[item] = text
+        return output
+
+            
+                
+    def meta_data():
+        first_step=["meta-data/"]
+        output=ec2.walk_the_tree(ec2.meta_data_url,first_step)
+        return output
+
+    def meta_data_json():
+        non_json_metadata=ec2.meta_data()
+        try:
+            json_metadata=ec2.json.dumps(non_json_metadata, indent=2)
+        except TypeError:
+            return non_json_metadata
+        return json_metadata
+
+    ## Function to pull desired data only
+    def desired_meta_date(desired_data):
+        first_step=['meta-data/']
+        desired_step=[first_step[0] + desired_data]
+        output=ec2.walk_the_tree(ec2.meta_data_url,desired_step)
+        return output
+
+
+
+
---
+++ challenge2/ec2_metadata.py
@@ -0,0 +1,18 @@
+import ec2_class as ec2
+
+print(ec2.meta_data_json())
+
+# code allows for a particular data key to be retrieved individually
+    #I have made it interactive 
+'''
+key=''
+while key != 'exit':
+    print("To exit the prog, type 'exit' ")
+    key=input("Which meta-data you want to pull \n -> ")
+    valid_inputs=['ami-id', 'ami-launch-index', 'ami-manifest-path', 'block-device-mapping/', 'events/', 'hostname', 'identity-credentials/', 'instance-action', 'instance-id', 'instance-life-cycle', 'instance-type', 'local-hostname', 'local-ipv4', 'mac', 'metrics/', 'network/', 'placement/', 'profile', 'public-hostname', 'public-ipv4', 'public-keys/', 'reservation-id', 'security-groups']
+        
+    if key in valid_inputs:
+        print(f'  {ec2_desired_meta_date(key)}  \n')
+    else:
+        print("Please input correct value ")
+'''
