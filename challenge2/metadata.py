69494eff5581ea70e4f99f587df95f6d1b90c32f
Akhilesh mishra <akhileshmishrabiz@gmail.com>
2022-06-26 19:25:37 +0530

    Added prog to make pulling ec2 metedata more user friendly

--- challenge2/metadata.py
+++ challenge2/metadata.py
@@ -2,6 +2,15 @@ import ec2_class
 import sys
 ec2=ec2_class.ec2()
 
+#This code will help user interact with this prog more effectively
+#to use this script, use python3 metadata.py <argument >
+# valid options are , json, and metadata variable in ec2 , or you can use --help for more infor
+#  python3 metadata.py --help
+# for pulling ec2 metadata in form of json use 
+# python3 metadata.py json
+#to get specific data use it as below
+# python3 metadata.py public-hostname
+
 valid_inputs=['ami-id', 'ami-launch-index', 'ami-manifest-path', 'block-device-mapping/', 'events/', 'hostname', 'identity-credentials/', 'instance-action', 'instance-id', 'instance-life-cycle', 'instance-type', 'local-hostname', 'local-ipv4', 'mac', 'metrics/', 'network/', 'placement/', 'profile', 'public-hostname', 'public-ipv4', 'public-keys/', 'reservation-id', 'security-groups']
 help_content= "-- Valid Arguments -- \n\n json  -It will give ec2 metadata in json format. \n\n Or Use specific keyword to get any specific ec2 metadata \n"
 try:
